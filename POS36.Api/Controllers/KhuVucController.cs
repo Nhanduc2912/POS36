c681ff08bce0172d615820386b34272451869fa8
Nhanduc2912 <nhanduc29122008@gmail.com>
2026-06-04 09:50:48 +0700

    update: Add authentication [Authorize]: Requires the API to receive a JWT Token to obtain the Shop Owner's login session identifier

--- POS36.Api/Controllers/KhuVucController.cs
+++ POS36.Api/Controllers/KhuVucController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
 using POS36.Api.Data;
 using POS36.Api.Models;
 
@@ -7,10 +8,13 @@ namespace POS36.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class KhuVucController : ControllerBase
     {
         private readonly AppDbContext _context;
         public KhuVucController(AppDbContext context) { _context = context; }
+
+        private int GetCuaHangId() => int.Parse(User.FindFirst("CuaHangId")!.Value);
 
         [HttpGet("chi-nhanh/{chiNhanhId}")]
         public async Task<IActionResult> GetByChiNhanh(int chiNhanhId)
@@ -24,7 +28,20 @@ namespace POS36.Api.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([FromBody] CreateKhuVucDto req)
         {
-            var newArea = new KhuVuc { ChiNhanhId = req.ChiNhanhId, TenKhuVuc = req.TenKhuVuc };
+            int cuaHangId = GetCuaHangId();
+
+            // Xác thực xem Chi nhánh có thuộc về Cửa hàng của user đăng nhập không
+            var chiNhanhHopLe = await _context.ChiNhanhs
+                .AnyAsync(cn => cn.Id == req.ChiNhanhId && cn.CuaHangId == cuaHangId);
+            if (!chiNhanhHopLe)
+                return BadRequest("Chi nhánh không thuộc cửa hàng hiện tại.");
+
+            var newArea = new KhuVuc 
+            { 
+                CuaHangId = cuaHangId, 
+                ChiNhanhId = req.ChiNhanhId, 
+                TenKhuVuc = req.TenKhuVuc 
+            };
             _context.KhuVucs.Add(newArea);
             await _context.SaveChangesAsync();
             return Ok(newArea);
